60aef9d7bd8d57f8203004256134963e7bee9ab4
spannondesh <spannohnderitu@gmail.com>
2025-09-07 17:37:06 +0300

    Create iris.py

---
+++ python week 7/iris.py
@@ -0,0 +1,18 @@
+import pandas as pd
+df=pd.read_csv('iris-parquet.csv')
+print(df.to_string())
+print(df.head(3))
+df=df.drop_duplicates()
+print(df.head())
+print(df.describe())
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+df.plot(kind='hexbin',x='petal.width',y='petal.length')
+plt.show()
+df.plot(kind='hist',x='petal.width',y='petal.length')
+plt.show()
+df.plot(kind='bar',x='petal.width',y='petal.length')
+plt.show()
+df.plot(kind='line',x='petal.width',y='petal.length')
+plt.show()
